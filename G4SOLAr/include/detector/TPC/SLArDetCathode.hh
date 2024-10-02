1de724894039a2f181adf764bb75a7448486b581
KRoss272 <s2175476@ed.ac.uk>
2024-10-02 22:06:07 +0100

    Adding ESR and PEN films

--- G4SOLAr/include/detector/TPC/SLArDetCathode.hh
+++ G4SOLAr/include/detector/TPC/SLArDetCathode.hh
@@ -15,25 +15,38 @@
 #include "G4VSolid.hh"
 #include "G4LogicalVolume.hh"
 #include "G4PVPlacement.hh"
+#include "G4LogicalSkinSurface.hh"
+#include "G4OpticalSurface.hh"
+#include "G4LogicalBorderSurface.hh"
+
+
 
 class SLArDetCathode : public SLArBaseDetModule {
 
 public:
   SLArDetCathode          ();
   virtual ~SLArDetCathode ();
+  
+  G4LogicalSkinSurface* BuildLogicalSkinSurface();
 
   void          BuildCathode();
 
   void          BuildMaterial(G4String);
   void          BuildDefalutGeoParMap();
+  
+  //G4LogicalSkinSurface* GetESRSkin() {return fSkinESRSurface;}
 
   G4ThreeVector GetCathodeCenter();
   virtual void  Init(const rapidjson::Value&) override; 
   void          SetVisAttributes();
-
+  
 private:
   // Some useful global variables
   SLArMaterial* fMatCathode;
+  SLArMaterial* fMatESR;
+  G4LogicalSkinSurface* fSkinESRSurface;
+  G4OpticalSurface* OpESRFilm;
+  //SLArBaseDetModule* fCathodeBuild; 
  
 };
 
