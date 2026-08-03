48dbf425e093f715755ae01b7d81dd8b997d8561
Daniele Guffanti <daniele.guffanti@cern.ch>
2026-08-03 12:20:45 +0200

    Adds check on backtracker time element

--- SOLArUtils/source/src/SLArEveOpHitRenderer.cc
+++ SOLArUtils/source/src/SLArEveOpHitRenderer.cc
@@ -250,6 +250,10 @@ namespace display {
 
         const auto& bt_coll = ev_xa.GetBacktrackerRecordCollection();
         if (bt_coll.empty()) continue;
+        if (bt_coll.find(time_bin) == bt_coll.end()) {
+          printf("Warning: time bin %d not found in backtracker collection for PDS wall %d\n", time_bin, idx_array);
+          continue;
+        }
         const auto& records = bt_coll.at(time_bin).GetConstRecords();
             
         // check if wavelength backtracker is active
